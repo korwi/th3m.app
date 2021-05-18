f65e8d0f2f52c43e6822b26b329fcd8115d1fe84
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-05-18 10:11:35 +0100

    Create the-thing-about-tensors.py

---
+++ 2021/the-thing-about-tensors.py
@@ -0,0 +1,60 @@
+## minute 1
+## minute 2
+## minute 3
+## minute 4
+## minute 5
+## minute 6
+## minute 7
+## minute 8
+## minute 9
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
+## minute 
